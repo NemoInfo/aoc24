862d2e33cc0f5cba4cb2810165521fc21ce14775
Aaron <aaronpanaitescu@gmail.com>
2024-12-04 08:13:55 +0000

    Improve day 2

--- day2/main.py
+++ day2/main.py
@@ -1,34 +1,23 @@
 import numpy as np
-from itertools import zip_longest
-
-FILENAME = "input.txt"
+file = open("input.txt", "r")
+nums = [np.array([*map(int, line.strip().split())]) \
+        for line in file.readlines()]
 
 ###############*###############
 #           PART 1            #
 ###############*###############
-file = open(FILENAME, "r")
-nums = np.array(list(zip_longest(*[[*map(int, \
-                  line.strip().split(" "))] for line in file.readlines()],
-                fillvalue=np.nan))).T
-
-diff = nums[:, :-1] - nums[:, 1:]
-nans = np.isnan(diff)
-
-res1 = (np.all(nans | (abs(diff) >= 1) & (abs(diff) <= 3), axis=1)
-        & (np.all(nans | (diff > 0), axis=1)
-           | np.all(nans | (diff < 0), axis=1))).sum()
+res1 = 0
+for xs in nums:
+  ds = xs[:-1] - xs[1:]
+  if np.all(abs(ds) >= 1) & np.all(abs(ds) <= 3) & \
+    (np.all(ds > 0) | np.all(ds < 0)):
+    res1 += 1
 
 print(f"Part 1: {res1}")
 
 ###############*###############
 #           PART 2            #
 ###############*###############
-file = open(FILENAME, "r")
-nums = [
-    np.array([*map(int,
-                   line.strip().split())]) for line in file.readlines()
-]
-
 res2 = 0
 for xs in nums:
   for i in range(len(xs)):
