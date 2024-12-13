2bc0cd46ecaa891d8b735244a7aaf97bbccf3f3f
Aaron <aaronpanaitescu@gmail.com>
2024-12-13 14:26:12 +0000

    Day 13 simplify input parsing

--- day13/main.py
+++ day13/main.py
@@ -1,33 +1,25 @@
-inpt = open("input.txt", "r").read().strip().split("\n\n")
+from re import findall
 
-
-def parse_game(lines):
-  a, b, p = lines.split("\n")
-  xa, ya = map(int, a[12:].split(", Y+"))
-  xb, yb = map(int, b[12:].split(", Y+"))
-  xp, yp = map(int, p[9:].split(", Y="))
-
-  return xa, ya, xb, yb, xp, yp
-
+inpt = open("input.txt", "r").read().split("\n\n")
 
 ###############*###############
 #           PART 1            #
 ###############*###############
 
 
-def compute_ab(xa, ya, xb, yb, xp, yp):
+def compute_tokens(xa, ya, xb, yb, xp, yp):
   assert (xb * ya - xa * yb) != 0
   b = (xp * ya - yp * xa) / (xb * ya - xa * yb)
   a = (xp - b * xb) / xa
 
-  return a, b
+  if a == int(a) and b == int(b):
+    return int(a) * 3 + int(b)
+  return 0
 
 
 res1 = 0
 for lines in inpt:
-  a, b = compute_ab(*parse_game(lines))
-  if a == int(a) and b == int(b):
-    res1 += int(a) * 3 + int(b)
+  res1 += compute_tokens(*map(int, findall(r"\d+", lines)))
 
 print(f"Part 1: {res1}")
 ###############*###############
@@ -36,9 +28,7 @@ print(f"Part 1: {res1}")
 
 res2 = 0
 for lines in inpt:
-  *rest, xp, yp = parse_game(lines)
-  a, b = compute_ab(*rest, xp + 10000000000000, yp + 10000000000000)
-  if a == int(a) and b == int(b):
-    res2 += int(a) * 3 + int(b)
+  *rest, xp, yp = [*map(int, findall(r"\d+", lines))]
+  res2 += compute_tokens(*rest, xp + 1e13, yp + 1e13)
 
 print(f"Part 2: {res2}")
